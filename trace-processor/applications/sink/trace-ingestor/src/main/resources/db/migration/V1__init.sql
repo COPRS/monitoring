d9979761e024dd7414477ac640827e5d0bc52680
Guillaume Durand <guillaume.durand@csgroup.eu>
2022-07-26 14:17:50 +0200

    feat: add ingestion mechanism for trace comming from metadata extraction service

--- trace-processor/applications/sink/trace-ingestor/src/main/resources/db/migration/V1__init.sql
+++ trace-processor/applications/sink/trace-ingestor/src/main/resources/db/migration/V1__init.sql
@@ -12,15 +12,35 @@ CREATE TABLE external_input
 
 CREATE TABLE dsib
 (
-    parent_id bigint NOT NULL REFERENCES external_input(id) ON DELETE CASCADE
+    parent_id bigint NOT NULL REFERENCES external_input(id) ON DELETE CASCADE,
+    PRIMARY KEY (parent_id)
 );
 
 CREATE TABLE chunk
 (
-    parent_id bigint NOT NULL REFERENCES external_input(id) ON DELETE CASCADE
+    parent_id bigint NOT NULL REFERENCES external_input(id) ON DELETE CASCADE,
+    dsib_id bigint NOT NULL REFERENCES dsib(parent_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (parent_id)
 );
+
+--ALTER TABLE chunk ADD CONSTRAINT dsib_chunk_constraint FOREIGN KEY (dsib_id) REFERENCES dsib(parent_id);
 
 CREATE TABLE aux_data
 (
-    parent_id bigint NOT NULL REFERENCES external_input(id) ON DELETE CASCADE
+    parent_id bigint NOT NULL REFERENCES external_input(id) ON DELETE CASCADE,
+    PRIMARY KEY (parent_id)
+);
+
+CREATE TABLE product
+(
+    id bigserial PRIMARY KEY,
+    filename VARCHAR NOT NULL UNIQUE,
+    custom json,
+    timeliness_name VARCHAR,
+    timeliness_value_seconds INTEGER,
+    end_to_end_product boolean,
+    duplicate boolean,
+    t0_pdgs_date TIMESTAMP,
+    prip_storage_date TIMESTAMP,
+    late boolean
 );
--- trace-processor/common/src/main/java/eu/csgroup/coprs/monitoring/common/datamodel/entities/ExternalInput.java
+++ trace-processor/common/src/main/java/eu/csgroup/coprs/monitoring/common/datamodel/entities/ExternalInput.java
@@ -1,6 +1,5 @@
 package eu.csgroup.coprs.monitoring.common.datamodel.entities;
 
-import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
 import eu.csgroup.coprs.monitoring.common.bean.AutoMergeableMap;
 import lombok.Data;
 import org.hibernate.annotations.*;
@@ -12,16 +11,13 @@ import java.time.Instant;
 @Data
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@TypeDefs({
-    @TypeDef(name = "pgsql_enum", typeClass = PostgreSQLEnumType.class),
-})
-public abstract class ExternalInput implements ClonableEntity {
+public abstract class ExternalInput extends DefaultEntity {
     @Id
     @SequenceGenerator(sequenceName="external_input_id_seq", name = "external_input_id_seq", allocationSize=1)
     @GeneratedValue(generator = "external_input_id_seq", strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    //@Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String filename;
 
     private String mission;
